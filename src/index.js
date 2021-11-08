7720b6168508057cc824a9fff5bfb55e8790dab0
Wilson Ortega <waortegaand@gmail.com>
2021-11-07 22:28:42 -0500

    Auth0: Se agrega autenticacion mediante cuenta google

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 //import reportWebVitals from './reportWebVitals';
-
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Auth0Provider
+    domain="dev-yqshildz.us.auth0.com"
+    clientId="YqrhzqgYdsrrXvbwikFRFNIBH0DOu4E1"
+    redirectUri={window.location.origin}
+    >
     <BrowserRouter>
-      <App />
+        <App />
     </BrowserRouter>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
